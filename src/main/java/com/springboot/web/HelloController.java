220ffc456c6b83f1341e6cc6e1e8b7a9b16bc568
kimnamgyu <lethargic.hood@gmail.com>
2023-02-18 15:07:10 +0900

    feat : helloDto 메서드 구현

--- src/main/java/com/springboot/web/HelloController.java
+++ src/main/java/com/springboot/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.springboot.web;
 
 
+import com.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
 
@@ -20,5 +27,9 @@ public class HelloController {
 @GetMapping
 HTTP Method인 Get의 요청을 받을 수 있게 API 생성
 -> /hello 요청이 오면 hello를 return
+
+@RequestParam
+와부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+
 
  */
