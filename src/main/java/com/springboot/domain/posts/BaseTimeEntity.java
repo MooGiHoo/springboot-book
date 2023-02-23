ae4750f7c1d444094cf6490f184d3f7700fa79f6
kimnamgyu <lethargic.hood@gmail.com>
2023-02-23 20:56:21 +0900

    feat : createdDate,modifiedDate 관리

---
+++ src/main/java/com/springboot/domain/posts/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package com.springboot.domain.posts;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
+
+/*
+
+모든 Entity의 상위 클래스가 되어 Entity들의 createdDate, modifiedDate 관리
+
+
+
+ */
