59f19cddb4e2c8bb237eed2de4ec88d008ea8aa0
kimnamgyu <lethargic.hood@gmail.com>
2023-02-23 17:58:55 +0900

    feat : API 요청 시 필요한 메서드 추가

---
+++ src/main/java/com/springboot/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package com.springboot.web;
+
+
+import com.springboot.service.posts.PostsService;
+import com.springboot.web.dto.PostsResponseDto;
+import com.springboot.web.dto.PostsSaveRequestDto;
+import com.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postService.findById(id);
+    }
+
+
+ }
